8b0b651f5999a4ff474952f102203e79a9e5205c
xunzai9 <55530058+xunzai9@users.noreply.github.com>
2026-01-28 13:37:43 +0800

    Update and rename nnhan01.js to nnhan02.js

---
+++ nnhan02.js
@@ -0,0 +1,15 @@
+(function() {
+    return {
+        name: "鸟鸟韩漫测试",
+        key: "nnhanman7_test",
+        version: "1.0.16",
+        minAppVersion: "1.0.0",
+        url: "https://nnhanman7.com",
+        searchOptions: [],
+        explore: [],
+        comic: {
+            loadInfo: function(id) { return Promise.resolve({}); },
+            loadEp: function(c, e) { return Promise.resolve({}); }
+        }
+    };
+})();
