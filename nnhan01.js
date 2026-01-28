786f405b3094b9224aca4c71ede1b1e13985ae8d
xunzai9 <55530058+xunzai9@users.noreply.github.com>
2026-01-28 13:30:06 +0800

    Update and rename nnhan.js to nnhan01.js

--- nnhan01.js
+++ nnhan01.js
@@ -2,7 +2,7 @@
     return {
         name: "鸟鸟韩漫",
         key: "nnhanman7",
-        version: "1.0.14",
+        version: "1.0.15",
         minAppVersion: "1.0.0",
         url: "https://nnhanman7.com",
         searchOptions: [],
@@ -10,7 +10,7 @@
         getHeaders: function() {
             return {
                 "Referer": "https://nnhanman7.com/",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+                "User-Agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36"
             };
         },
 
