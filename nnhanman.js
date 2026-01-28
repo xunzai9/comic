1bc7cc0c164db6d88a5476eac58083a17dba7b0a
xunzai9 <55530058+xunzai9@users.noreply.github.com>
2026-01-28 11:02:01 +0800

    Update nnhanman.js

--- nnhanman.js
+++ nnhanman.js
@@ -1,75 +1,55 @@
+// @ts-ignore
 var source = {
     name: "鸟鸟韩漫",
     key: "nnhanman7",
-    version: "1.0.6",
+    version: "1.0.7",
     minAppVersion: "1.0.0",
     url: "https://nnhanman7.com",
     searchOptions: [],
-
     getHeaders: function() {
-        return {
-            "Referer": "https://nnhanman7.com/",
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
-        };
+        return { "Referer": "https://nnhanman7.com/", "User-Agent": "Mozilla/5.0 (Mobile) Safari/537.36" };
     },
-
     explore: [{
-        title: "最新更新",
+        title: "首页",
         type: "multiPartPage",
-        load: async function() {
-            try {
-                var res = await Network.get("https://nnhanman7.com", { headers: this.getHeaders() });
-                var comics = [];
-                var regex = /<a[^>]+href="([^"]+)"[^>]*title="([^"]+)"[\s\S]*?<img[^>]+src="([^"]+)"/g;
-                var match;
-                while ((match = regex.exec(res)) !== null) {
-                    var id = match[1];
-                    if (id.indexOf('/comic/') !== -1) {
-                        comics.push({
-                            id: id,
-                            title: match[2],
-                            cover: match[3]
-                        });
+        load: function() {
+            return Network.get("https://nnhanman7.com", { "Referer": "https://nnhanman7.com/" })
+                .then(function(res) {
+                    var comics = [];
+                    var regex = /<a[^>]+href="([^"]+)"[^>]*title="([^"]+)"[\s\S]*?<img[^>]+src="([^"]+)"/g;
+                    var match;
+                    while ((match = regex.exec(res)) !== null) {
+                        if (match[1].indexOf('/comic/') !== -1) {
+                            comics.push({ id: match[1], title: match[2], cover: match[3] });
+                        }
                     }
-                }
-                return [{ title: "首页更新", comics: comics }];
-            } catch (e) {
-                return [];
-            }
+                    return [{ title: "首页推荐", comics: comics }];
+                });
         }
     }],
-
     comic: {
-        loadInfo: async function(id) {
-            var res = await Network.get("https://nnhanman7.com" + id, { 
-                headers: { "Referer": "https://nnhanman7.com/" } 
-            });
-            var chapters = [];
-            var chapterRegex = /href="([^"]+)"[^>]*>([\s\S]*?第[\s\S]*?话[\s\S]*?)<\/a>/g;
-            var m;
-            while ((m = chapterRegex.exec(res)) !== null) {
-                chapters.push({
-                    id: m[1],
-                    title: m[2].replace(/<[^>]+>/g, "").trim()
+        loadInfo: function(id) {
+            return Network.get("https://nnhanman7.com" + id, { "Referer": "https://nnhanman7.com/" })
+                .then(function(res) {
+                    var chapters = [];
+                    var regex = /href="([^"]+)"[^>]*>([\s\S]*?第[\s\S]*?话[\s\S]*?)<\/a>/g;
+                    var m;
+                    while ((m = regex.exec(res)) !== null) {
+                        chapters.push({ id: m[1], title: m[2].replace(/<[^>]+>/g, "").trim() });
+                    }
+                    return { title: "详情", chapters: chapters };
                 });
-            }
-            return { title: "漫画详情", chapters: chapters };
         },
-        loadEp: async function(comicId, epId) {
-            var res = await Network.get("https://nnhanman7.com" + epId, { 
-                headers: { "Referer": "https://nnhanman7.com/" } 
-            });
-            var images = [];
-            var imgRegex = /img[^>]+src="([^"]+)"/g;
-            var m;
-            while ((m = imgRegex.exec(res)) !== null) {
-                if (m[1].indexOf('jmpic') !== -1) images.push(m[1]);
-            }
-            return { images: images };
+        loadEp: function(cId, eId) {
+            return Network.get("https://nnhanman7.com" + eId, { "Referer": "https://nnhanman7.com/" })
+                .then(function(res) {
+                    var images = [];
+                    var m, regex = /img[^>]+src="([^"]+)"/g;
+                    while ((m = regex.exec(res)) !== null) {
+                        if (m[1].indexOf('jmpic') !== -1) images.push(m[1]);
+                    }
+                    return { images: images };
+                });
         }
-    },
-
-    onTagSuggestionSelected: function(keyword) {
-        return null;
     }
 };
